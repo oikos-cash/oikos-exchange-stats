642f468719252a2f59d5a5a84c27344d13ffc7f7
Oikos.cash <albert@oikos.cash>
2021-02-05 17:08:38 -0400

    add cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const getTotalSupply = require('./total-supply');
 const getPairs = require('./pairs');
@@ -7,12 +8,16 @@ const getTotalLocked = require('./total-locked');
 
 const router = express.Router();
 
+const corsOptions = {
+	origin: 'https://accounts.coinmarketcap.com',
+  }
+
 router.get('/alive', (req, res) => {
 	res.send('API is live');
 });
 
-router.get('/api/total-locked', getTotalLocked);
-router.get('/api/total-supply', getTotalSupply);
+router.get('/api/total-locked',  getTotalLocked);
+router.get('/api/total-supply', cors(corsOptions), getTotalSupply);
 router.get('/api/pairs/:category', getPairs);
 router.get('/api/ticker/:pair', getTicker);
 
